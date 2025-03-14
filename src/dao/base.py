704d8cbf136983b509b9e7adf347ee0d89a56e28
traddelexx <traddelexx@gmail.com>
2025-03-14 21:21:00 +0200

    Added Delete and Update func

--- src/dao/base.py
+++ src/dao/base.py
@@ -1,22 +1,36 @@
 from src.database import async_session
-from sqlalchemy import select, insert
+from sqlalchemy import delete, select, insert, update
 
 
 class BaseDAO:
     model = None
 
     @classmethod
-    async def find_all(cls, **filter_by):
+    async def find_all_func(cls, **filter_by):
         async with async_session() as session:
             query = select(cls.model).filter_by(**filter_by)
             result = await session.execute(query)
             return result.scalars().all()
 
     @classmethod 
-    async def add(cls, data: dict):
+    async def add_func(cls, data: dict):
         async with async_session() as session:
             stmt = insert(cls.model).values(**data)
             await session.execute(stmt)
+            await session.commit()
+
+    @classmethod 
+    async def delete_func(cls, obj_id: int):
+        async with async_session() as session:
+            query = delete(cls.model).where(cls.model.id == obj_id)
+            await session.execute(query)
+            await session.commit()
+
+    @classmethod
+    async def update_func(cls, obj_id: int, data: dict):
+        async with async_session() as session:
+            query = update(cls.model).where(cls.model.id == obj_id).values(**data)
+            await session.execute(query)
             await session.commit()
 
 
--- src/task/router.py
+++ src/task/router.py
@@ -1,6 +1,6 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends
-from src.task.schemas import Tasks_Schema
+from fastapi import APIRouter, Depends, Path
+from src.task.schemas import Tasks_Schema, Tasks_SchemaID
 from src.task.base import TasksDAO
 
 router = APIRouter(
@@ -10,11 +10,24 @@ router = APIRouter(
 
 @router.get('')
 async def get_all_tasks() -> list[Tasks_Schema]:
-    return await TasksDAO.find_all()
+    return await TasksDAO.find_all_func()
 
 @router.post('')
 async def add_task(
     task: Annotated[Tasks_Schema, Depends()]
 ):
-    await TasksDAO.add(task.model_dump())
+    await TasksDAO.add_func(task.model_dump())
+    return {'ok': True}
+
+@router.delete('/{task_id}')
+async def delete_task(task_id: int):
+    await TasksDAO.delete_func(task_id)
+    return {'ok': True}
+
+@router.put('/{task_id}')
+async def update_task(
+    task_id: Annotated[int, Path()],  
+    task: Annotated[Tasks_Schema, Depends()] 
+):
+    await TasksDAO.update_func(task_id, task.model_dump())
     return {'ok': True}
